5063ae7bf069c065dc9a7e3c00e59d5e1ac53dfa
Hannan <najma.mahat@student.moringaschool.com>
2023-03-18 17:38:18 +0300

    Completed countdown to midnight

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown n
+    x = 0
+    while x < n
+        puts "#{n} SECOND(S)!"
+        n -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep n
+    x = 0
+    while x < n
+        puts "#{n} SECOND(S)!"
+        sleep 1
+        n -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
